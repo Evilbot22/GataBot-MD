e165779e572a6a76aac03cb0fec456858045f57f
Evilbot22 <salomepremium13@gmail.com>
2025-02-06 01:56:01 -0500

    Evil_Bot-MD

--- config.js
+++ config.js
@@ -13,17 +13,8 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["5214774444444", '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿
-["5492266613038"], 
-["5573031859"],
-["5218715746374"],
-["593968585383"],
-["595976126756"],
-["584125778026"],
-["593968263524"],
-["5215539356057"],
-["573012482597"],
-["51968374620"]]
+["3234313092", 'Evil_Bot', true], //Evil_𝘽𝙤𝙩-𝙈𝘿
+["3006628213"],
 
 global.mods = []
 global.prems = []
@@ -58,7 +49,7 @@ global.confirmCode = "" // No tocar esto : Do not touch this line
 
 // ❰❰ IDIOMAS DISPONIBLES : AVAILABLE LANGUAGES ❱❱
 // Español 👉 es           
-// English 👉 en
+// English 👉 es
 global.lenguajeGB = es
 global.mid = esDefault
 global.version_language = '1.0 (MID-GB)'
